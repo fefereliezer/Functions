46d136defb1a04de4d60e15f2091a1791ed622f4
e f <fefereliezer@gmail.com>
2020-11-09 18:32:26 +0100

    Functions

---
+++ Functions/src/main/java/com/solutions/ef/functions/Functions.java
@@ -0,0 +1,17 @@
+package com.solutions.ef.functions;
+
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Functions {
+    public static void setText(@NonNull TextView tv, @Nullable String string){
+        if(string == null){
+            tv.setText("");
+        }else {
+            tv.setText(string);
+        }
+
+    }
+}
